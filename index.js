ff69d72483ae77dfef0130144b6dfd3b79c8c319
Swapnil Gawali <112171267+Swapnil-Devops@users.noreply.github.com>
2023-07-04 12:47:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ async function run() {
      const token = core.getInput('token');
     const octokit = github.getOctokit(token);
     console.log("inside run", packageName );
-    const { data: metadata } = await octokit.request('GET /pkgs/container/my-package', {
+    const { data: metadata } = await octokit.request('GET /pkgs/container/my-package:sha-6091f33', {
       package_name: packageName
     });
     console.log("metadata", metadata,JSON.stringify(metadata));
